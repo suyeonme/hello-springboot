53ceff5d9b2f18b7b01c68fcf885622b6fb425f5
Suyeon Kang <sykang@whatap.io>
2022-12-30 18:18:11 +0900

    Add ItemRepository and test on it

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,44 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * HashMap: 멀티쓰레드 환경에서는 ConcurrentHashMap 사용
+ * Long: 멀티쓰레드 환경에서는 AtomicLong 사용
+ */
+
+@Repository
+public class ItemRepository {
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
